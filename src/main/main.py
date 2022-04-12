ed241324006bb6deb67203247f9017a4b06be247
cshields27 <cshield6@nd.edu>
2022-04-12 18:04:26 -0400

    more setup

--- src/main/main.py
+++ src/main/main.py
@@ -5,8 +5,14 @@ Run to activate venv:
 source /escnfs/home/cmc/public/venv/bin/activate
 '''
 
-def main():
-  pass
+def read_question():
+  return input('- ')
 
+def main():
+  print('Welcome to the IDS Spring 2022 NBA Dialogue System version 1.0')
+  print('Getting things ready...')
+  while (question := read_question()):
+    pass
+    
 if __name__ == '__main__':
   main()
