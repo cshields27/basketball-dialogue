064084c7666d757abc2f42c801876ac58a7e03d4
kenan_lumantas <klumanta@nd.edu>
2022-04-28 17:21:38 -0400

    Make new context with averages

--- src/datagen/make_contexts.py
+++ src/datagen/make_contexts.py
@@ -28,41 +28,49 @@ from nba_api.stats.endpoints import playercareerstats
 
 def generate_contexts():
   # Open file
-  f = open('../../data/contexts.test', 'w')
+  f = open('../../data/contexts_original_temp.test', 'w')
 
   # Go through each player
   for player in players.get_players():
-    time.sleep(.5) # sleep so the api/NBA stats doesn't kick us out
-    # Access API endpoints
-    common_player_info = commonplayerinfo.CommonPlayerInfo(player_id=player['id'], timeout=100)
-    career_stats = playercareerstats.PlayerCareerStats(player_id=player['id'])
+    try:
+      time.sleep(.5) # sleep so the api/NBA stats doesn't kick us out
+      # Access API endpoints
+      common_player_info = commonplayerinfo.CommonPlayerInfo(player_id=player['id'], timeout=50)
+      career_stats = playercareerstats.PlayerCareerStats(player_id=player['id'])
 
-    # Get common player info as well as regular season stats
-    player_info = common_player_info.common_player_info.get_dict()
-    reg_season_stats = career_stats.season_totals_regular_season.get_dict()
-    # print(player_info)
-    # print(reg_season_stats)
+      # Get common player info as well as regular season stats
+      player_info = common_player_info.common_player_info.get_dict()
+      player_stats = common_player_info.player_headline_stats.get_dict()
+      reg_season_stats = career_stats.season_totals_regular_season.get_dict()
 
-    # Write contexts to contexts.test 
-    # Get player info and stats dicts
-    player = dict()
-    player_info = dict(zip(player_info['headers'], player_info['data'][0]))
-    career_data = dict()
+      # print(player_info)
+      # print(player_stats)
+      # print(reg_season_stats)
 
-    # Get player stats for every year
-    for year_data in reg_season_stats['data']:
-      year_data = dict(zip(reg_season_stats['headers'], year_data))
-      career_data[year_data['SEASON_ID']] = year_data
+      # Write contexts to contexts.test 
+      # Get player info and stats dicts
+      player = dict()
+      player_info = dict(zip(player_info['headers'], player_info['data'][0]))
+      career_data = dict()
+      averages = dict(zip(player_stats['headers'], player_stats['data'][0]))
 
-    # Write dict to contexts file
-    player['info'] = player_info
-    player['career_data'] = career_data
+      # Get player stats for every year
+      for year_data in reg_season_stats['data']:
+        year_data = dict(zip(reg_season_stats['headers'], year_data))
+        career_data[year_data['SEASON_ID']] = year_data
 
-    f.write('STARTTAG> {} ENDTAG\n'.format(str(player)))
+      # Write dict to contexts file
+      player['info'] = player_info
+      player['career_data'] = career_data
+      player['averages'] = averages
 
-    name = player_info['DISPLAY_FIRST_LAST']
+      f.write('STARTTAG {} ENDTAG\n'.format(str(player)))
 
-    print(name, 'added to contexts.test')
+      name = player_info['DISPLAY_FIRST_LAST']
+
+      print(name, 'added to contexts.test')
+    except:
+      continue
 
   f.close()
 
